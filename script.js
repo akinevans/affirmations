bc54d86b536d2142f010182869ee08bf99b5a7bc
Akin Evans <akinevans@gmail.com>
2021-10-17 12:28:43 -0700

    Linked first 2 Btns to JS file and wrote a basic function.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+var happyBtn = document.getElementById("happyBtn");
+
+var excitedBtn = document.getElementById("excitedBtn");
+
+// function call wont work if you use () with the onclick assignment. Example:
+//happyBtn.onclick = msg(); <-- wont work
+
+happyBtn.onclick = msg;
+excitedBtn.onclick = msg;
+
+function msg() {
+  console.log("btn works");
+}
