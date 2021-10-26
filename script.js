a9f548a2cc3cef33224ebd3e6b9dcb96c6a391d8
Akin Evans <akinevans@gmail.com>
2021-10-26 14:56:36 -0700

    write initial variables in JS document

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-console.log("test");
-
+//Declare button variables
 var happyBtn = document.getElementById("happyBtn");
+var calmBtn = document.getElementById("calmBtn");
+var sadBtn = document.getElementById("sadBtn");
+var nervousBtn = document.getElementById("nervousBtn");
+var hurtBtn = document.getElementById("hurtBtn");
+var angryBtn = document.getElementById("angryBtn");
 
-var excitedBtn = document.getElementById("excitedBtn");
+//Declare message arrays
+var happyMessages = [
+  "When I feel happy I manifest more reasons to be happy.",
+  "I accept that happiness is my true nature.",
+  "I choose to pursue peace and harmony every day of my life.",
+  "I am proud of myself.",
+  "Am willing to accept myself unconditionally",
+];
 
-// function call wont work if you use () with the onclick assignment. Example:
-//happyBtn.onclick = msg(); <-- wont work
+var calmMessages = [
+  "All I need to do right now is breathe.",
+  "My need to feel calm is being fully met.",
+  "Everything is well in this moment.",
+  "I love and accept myself.",
+  "I am comfortable and satisfied with myself.",
+];
 
-happyBtn.onclick = msg;
-excitedBtn.onclick = msg;
+var sadMessages = [
+  "I pursue happiness regardless of the circumstances.",
+  "My sadness does not define me.",
+  "I am in charge of my life, and my happiness and I feel happy and content.",
+  "Not everybody will understand my situation, and thats okay.",
+  "I am a work in progress, and I welcome every positive change.",
+];
 
-function msg() {
-  console.log("btn works");
-}
+var nervousMessages = [
+  "I am resilient in the face of any challenge.",
+  "I am strong and I am enough.",
+  "Just like before, Ill survive this situation.",
+  "My past experiences cant stop me from succeeding in the future.",
+  "All is well with me.",
+];
+
+var hurtMessages = [
+  "Negative opinions have no place in my mind.",
+  "Good things belong to me.",
+  "I am not defined by my past pain and hurts.",
+  "I have patience with myself.",
+  "I am able to love and receive love.",
+];
+
+var angryMessages = [
+  "I can feel my anger and still stay in control.",
+  "I can do better next time.",
+  "I can express my anger in a respectful way.",
+  "I remember to breathe deeply when I feel angry.",
+  "This anger is temporary.",
+];
+//Define text area
+
+//Declare function
+// Declare variable for event target
+//Declare variable for generating random number
+
+console.log(angryMessages[3]);
